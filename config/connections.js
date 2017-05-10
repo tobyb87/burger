8357cf8853929e1a5e0988e341fe25ce026d157a
Toby Boyd <tobyboyd87@gmail.com>
2017-05-10 14:26:26 -0500

    removing heroku database

--- config/connections.js
+++ config/connections.js
@@ -1,12 +1,8 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection;
 
-if (process.env.JAWSDB_"mysql://l3s1as7wnvdxx9ny:wbulxijpkjwoweyr@o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/iarh0u6f3nu4gufv"); {
-  connection = mysql.createConnection(process.env.JAWSDB_"mysql://l3s1as7wnvdxx9ny:wbulxijpkjwoweyr@o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/iarh0u6f3nu4gufv");
-} else{
 
- connection= mysql.createConnection({
+var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,6 @@
+USE burgers;
 
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
-CREATE TABLE burgers
+CREATE TABLE the_burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(40) NOT NULL,
